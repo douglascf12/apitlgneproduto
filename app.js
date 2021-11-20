2d6a1793dc6a7f27a19f3e715a295a46a6b48324
Douglas Cardoso <douglascf12@live.com>
2021-11-20 14:09:03 -0300

    criar rotas dos produtos

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express')
-app = express()
+const app = express()
 
-app.use((req, res, next) => {
-    res.status(200).send({
-        mensagem: 'Ok, deu certo!'
-    })
-})
+const rotaProdutos = require('./routes/produtos')
+
+app.use('/produtos', rotaProdutos)
 
 module.exports = app
